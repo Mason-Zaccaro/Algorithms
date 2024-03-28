9d1349f82e71842623340c77a0e4066d0d3241ac
Mason Zaccaro <65304987+Mason-Zaccaro@users.noreply.github.com>
2024-03-28 17:12:15 +0300

    Add files via upload

---
+++ 20.03 Задание 2.1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+//задание: Используя стек, напечатать символы некоторой величины строкового типа в обратном порядке.
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine();
+        string reversString = ReverseString(inputString);
+        Console.WriteLine($"Вводная строка: {inputString}");
+        Console.WriteLine($"Перевернутая строка: {reversString}");
+    }
+
+    static string ReverseString(string input)
+    {
+        Stack stack = new Stack();
+        foreach (char x in input)
+        {
+            stack.Push(x);
+        }
+
+        char[] reversedChars = new char[input.Length];
+        int index = 0;
+        while (stack.Count > 0)
+        {
+            reversedChars[index++] = (char)stack.Pop();
+        }
+
+        return new string(reversedChars);
+    }
+}
